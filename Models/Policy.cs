edae21a36a30f864dc807aaaf7fe92f46f7ffa8f
codeitp <patrobaoteko@gmail.com>
2025-01-17 22:00:51 +0300

    updated files

---
+++ Models/Policy.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace InsurancePolicyAPI.Models
+{
+    public class Policy
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string PolicyName { get; set; } = string.Empty;
+
+        [Range(1, int.MaxValue)]
+        public decimal Premium { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public decimal CoverageAmount { get; set; }
+
+        [Required]
+        public string PolicyHolderName { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime EffectiveDate { get; set; }
+    }
+
+
+
+}
